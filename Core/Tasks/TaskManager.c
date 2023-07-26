b4948b7702fd0fbc3719169c69eea91582eb1916
jeremycote <jeremymcote@gmail.com>
2023-07-25 21:25:44 -0400

    Move lv_conf.h to Inc

--- Core/Tasks/TaskManager.c
+++ Core/Tasks/TaskManager.c
@@ -4,15 +4,9 @@
 
 // Can be removed if causing compilation issues on other systems
 #include "TaskManager.h"
-#include "lvgl/lvgl.h"
 #include "LVGLTimerTask.h"
 
 void RunTaskManager(void) {
     DebugPrint("Starting Task Manager...");
-
-//    while(1) {
-//        lv_task_handler();
-//        HAL_Delay(5);
-//    }
     InitLVGLTimerTask();
 }
