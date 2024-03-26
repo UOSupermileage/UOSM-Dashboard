4e0e7ec6f1268e9aade8c343ff4b4799b98cb9d7
Jeremy <jeremymcote@gmail.com>
2024-03-25 21:34:12 -0400

    Squashed 'UOSM-Core/' changes from 96b12c0..18dcdac
    18dcdac Add guard to CAN
    3a75756 Update brakes CAN
    826be0b Add breaks to CAN
    370eed7 Merge commit '898d4ed5a17bdbfb820afaa624f32bbd084a6cb0'
    3f17b09 Added brakelights to light status.
    5ff0e8a Merged core.
    10fc511 Cleanup
    978b97c Working CAN bus
    3b4412c Save
    d7ec8a3 Improve bar chart visibility, working can
    2bfa148 Merge UOSM-Core
    4085a3f Merge commit 'd6b49e6b19f1c31fddd185e82021301ad48af1ab'
    a303fad Simplify
    1bb49e9 Implement ReadPressureTemperatureMessage
    d88a085 Merge commit '6657f90abe465c48434a5850e01b287b5e67be4e'
    2be0abc Move CAN to main
    daf2dd5 Working Prints
    bd4f5eb Merge commit 'f19c1e466f988a4c3c59c5cd2a3bb3ad10cd5b39'
    b4c41b2 Add eventcode types and callback for countdown
    
    git-subtree-dir: UOSM-Core
    git-subtree-split: 18dcdac1dab3e78c8d336d22e129321100d31032

--- Inc/ApplicationTypes.h
+++ Inc/ApplicationTypes.h
@@ -53,7 +53,8 @@ typedef enum {
     MOTOR_INITIALIZING,
     UNDERVOLTAGE,
     DRIVER_ENABLED,
-    NEW_LAP
+    NEW_LAP,
+    BRAKES_ENABLED
 } EventCode;
 
 typedef enum {
@@ -72,7 +73,7 @@ typedef uint16_t length_t;
 typedef uint16_t percentage_t;
 typedef int32_t velocity_t;
 typedef uint16_t throttle_raw_t;
-typedef uint16_t voltage_t;
+typedef float voltage_t;
 typedef uint16_t speed_t;
 typedef uint16_t seconds_t;
 typedef uint32_t ms_t;
@@ -106,24 +107,23 @@ typedef struct {
 typedef union {
     uint32_t all;
     struct {
-        uint32_t hazards_enabled:1;
-        uint32_t left_turn_enabled:1;
-        uint32_t right_turn_enabled:1;
-        uint32_t headlights_enabled:1;
-        uint32_t low_beams_enabled:28;
+        uint32_t hazards_enabled: 1;
+        uint32_t left_turn_enabled: 1;
+        uint32_t right_turn_enabled: 1;
+        uint32_t headlights_enabled: 1;
+        uint32_t low_beams_enabled: 28;
     };
 } lights_status_t;
 
 typedef union {
     uint32_t all;
     struct {
-        uint32_t lap_0:8;
-        uint32_t lap_1:8;
-        uint32_t lap_2:8;
-        uint32_t lap_3:8;
+        uint32_t lap_0: 8;
+        uint32_t lap_1: 8;
+        uint32_t lap_2: 8;
+        uint32_t lap_3: 8;
     };
 } lap_efficiencies_t;
-
 #ifdef __cplusplus
 }
 #endif
